9431b267369617335d6080fc7424ea8ba362ff62
unknown <nmallow@NMALLOW-990.NRPI.LOCAL>
2013-05-21 09:56:43 -0800

    nm - added col loc selection

--- google-api-tran-to-dict/csvconvert.py
+++ google-api-tran-to-dict/csvconvert.py
@@ -23,13 +23,45 @@ FILE_OUT = 'string_to_dict_%s.txt ' % date.today()
 
 
 KEYCOL_KEY = 'tagcode'
-KEYCOL_US  = 'en-us'
+AVAILABLE_LCID = ['en-us','en-ca','en-gb']
+KEYCOL_DEFAULT = 'en-us'
+KEYCOL_USE = ''
 
 
+def determine_loc(rec=False,value=None):
+    global KEYCOL_USE
+    if rec == True:
+        print 'The value you have entered was invalid, Please try again.\n'
+
+    #build array string value
+    output_str = ''
+    for k,v in enumerate(AVAILABLE_LCID):
+        output_str = ''.join([output_str,"[%d:%s]," % (k,v)])
 
 
+    loc = raw_input("Available translations: \n%s\n Enter a number or press 'enter' to use default '%s' " % (output_str,KEYCOL_DEFAULT,))
+
+    item = loc if loc else KEYCOL_DEFAULT
+
+
+    try:
+        KEYCOL_USE = AVAILABLE_LCID[int(item)]
+    except IndexError:
+        determine_loc(True)
+    except ValueError:
+        if KEYCOL_DEFAULT == item:
+            KEYCOL_USE = KEYCOL_DEFAULT
+        else:
+            determine_loc(True)
+
+    return
 
 def csv_to_txt(infile, delimiter=","):
+
+    #choose column
+    determine_loc()
+
+    #run run run
     result = {}
     reader = csv.reader(infile, delimiter=delimiter)
 
@@ -51,13 +83,13 @@ def csv_to_txt(infile, delimiter=","):
 
     text_file.writelines('{\n')
     for row in reader:
-        text_file.writelines(str('"'+row[KEYCOL_KEY]+'"="'+row[KEYCOL_US]+ '",\n'))
+        text_file.writelines(str('"'+row[KEYCOL_KEY]+'"="'+row[KEYCOL_USE]+ '",\n'))
         #result[row['tagcode']]=row['value']
     text_file.writelines('}')
 
 
 def request_input():
-    doc = raw_input("Please input the filename you wish to parse or press enter to use default '%s' " % FILE_IN)
+    doc = raw_input("Please input the filename you wish to parse or press 'enter' to use default '%s' " % FILE_IN)
     try:
         file = doc if doc else FILE_IN
         validate_input(file)
